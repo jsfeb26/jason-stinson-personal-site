57a2e755ee9639bf997bd619054e2ef20e0fd73e
Jason Stinson <jstinson@atlassian.com>
2018-05-05 18:51:59 -0400

    test

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,6 +5,18 @@ const Projects = () => (
   <div>
     <h1>Projects</h1>
     <img style={{ height: '100%', width: '100%' }} src={underConstruction} />
+
+    <form name="test" netlify>
+      <p>
+        <label>Name <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Email <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </div>
 )
 
